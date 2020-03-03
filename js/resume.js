a0a21e6d578c78536e41b6f1feab9b7e78e2d479
Wallace Ramos <wallacegr.sk8@gmail.com>
2020-03-03 11:45:12 -0300

    light or dark theme selector

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,15 @@
-          jQuery(document).ready(function($) { 
-    $(".scroll").click(function(event){        
+
+
+jQuery(document).ready(function ($) {
+    $(".scroll").click(function (event) {
         event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
-   });
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 700);
+    });
 });
+const sunMoonContainer = document.querySelector('.sun-moon-container')
+
+document.querySelector('.theme-toggle-button').addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+  const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'))
+  sunMoonContainer.style.setProperty('--rotation', currentRotation + 180)
+})
